aed79322c7e451b8782adf7f425de38f6f36fb3b
i20100 <20100@gmx.ch>
2017-06-07 23:56:59 +0200

    added test cases
    compare codeToString == codeToString
    and
    compare codeToString == codeToString2

--- JAV-ZF-II/src/mastermind/IntToString.java
+++ JAV-ZF-II/src/mastermind/IntToString.java
@@ -3,19 +3,31 @@ package mastermind;
 public class IntToString {
 
 	static int[] code = {1, 1, 4, 2}; // Init Variable Code
+	static int[] code2 = {1, 1, 4, 2}; // Init Variable Code
+//	String a = codeToString;
+//	String b = codeToString2;
 
 	public static void main(String[] args) {
-		// Testen von Int to String in Schleife
-
+		/* Testen von Int to String in Schleife. Resultat: true! aber wieso?
+		 * Versuch ausweiten! Zeile code2 hinzugefügt und Vergleich
+		 * codeToString == codeToString2 => Resultat: false!
+		 * 3. Versuch: Prüfen umweg über variablen:
+		 * Bsp. String a = code, String b = code2?*/
 
 		String codeToString = "";
 		for (int i = 0; i < code.length; i++) {
 			codeToString = codeToString + Integer.toString(code[i]);
-//			Those two commentlines are for test only!
-//			System.out.println(codeToString);
 		}
-System.out.println(codeToString);
+		String codeToString2 = "";
+		for (int i = 0; i < code2.length; i++) {
+			codeToString2 = codeToString2 + Integer.toString(code2[i]);
+		}
+		System.out.println("codeToString: " + codeToString);
+		System.out.println("codeToString2: "+ codeToString2);
+		System.out.println("Vergleich von codeToString == codeToString");
+		System.out.println(codeToString == codeToString);
+		System.out.println("Vergleich von codeToString == codeToString2");
+		System.out.println(codeToString == codeToString2);
 
 	}
-
 }
