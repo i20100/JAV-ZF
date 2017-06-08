ac61e86f11f16fcc54b369659a525bddbac48843
i20100 <20100@gmx.ch>
2017-06-08 15:28:29 +0200

    update mastermind
    get "tip" (guess of code) from user

--- JAV-ZF-II/src/test/Mastermind.java
+++ JAV-ZF-II/src/test/Mastermind.java
@@ -1,25 +1,30 @@
 package test;
 
+import java.util.Scanner;
+
 public class Mastermind {
 	static int[] codeArray = {0, 0, 0, 0}; // Init Variable Code
-	
+	static int[] tipArray = {0, 0, 0, 0}; // Variable für Benutzer Tipp
+
 	public static void main(String[] args) {
+		
+		tipUser(tipArray);
+
 
 //		generate 4 numbers and write them to codeArray
 		for (int i = 0; i < codeArray.length; i++) {
 			double random = Math.random() * 6 + 1;
 			codeArray[i] = (int)random;
-			System.out.println((int)random); // Prints random as int, Note the downcast	
 		}
 		
-//		test codeArray
+//		test codeArray and tipArray
 		System.out.println(arrayToString(codeArray));
+		System.out.println(arrayToString(tipArray));
 		
-//		convert array to string tipToString() exists
-//		save Tip to Array int[] arrayTip
+	
 	}
 
-	
+//	Array to "xxxx" String converter
 	static String arrayToString(int[] array) {
 		String arrayToString = "";
 		for (int i = 0; i < array.length; i++) {
@@ -27,4 +32,20 @@ public class Mastermind {
 		}
 		return arrayToString;
 	}
+	
+	
+	/** Aks User for his guess and gets the Userinput. 
+	 * @param tipArray
+	 */
+	public static void tipUser(int[] tipArray) {
+		System.out.println("Bitte geben Sie vier Zahlen zwischen 1-6 nach einander ein. Jede Zahl gefolgt von Enter.");
+		// TODO Zahleneingabe ohne Enter nach jeder Zahl ermöglichen
+		// TODO close reader
+		Scanner reader = new Scanner(System.in);
+		for (int i = 0; i < tipArray.length; i++) {
+			tipArray[i] = reader.nextInt();
+		}
+	}
+	
+	
 }
