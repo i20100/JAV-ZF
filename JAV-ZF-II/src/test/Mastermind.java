cb243731209bc42ed228efa866b32cda6f7b46ee
i20100 <20100@gmx.ch>
2017-06-08 14:35:20 +0200

    add create one random number

---
+++ JAV-ZF-II/src/test/Mastermind.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Mastermind {
+
+	public static void main(String[] args) {
+		
+		double random = Math.random() * 6 + 1;		
+		System.out.println(random); // Prints random as double
+		System.out.println((int)random); // Prints random as int, Note the downcast
+//		convert array to string tipToString() exists
+//		save Tip to Array int[] arrayTip
+	}
+
+}
