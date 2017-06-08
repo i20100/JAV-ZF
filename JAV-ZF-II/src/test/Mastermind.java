db92537c73ff34585df2830ea837ccd0650aaf0d
i20100 <20100@gmx.ch>
2017-06-08 14:38:44 +0200

    update mastermind
    generate 4 numbers

--- JAV-ZF-II/src/test/Mastermind.java
+++ JAV-ZF-II/src/test/Mastermind.java
@@ -3,10 +3,12 @@ package test;
 public class Mastermind {
 
 	public static void main(String[] args) {
-		
-		double random = Math.random() * 6 + 1;		
-		System.out.println(random); // Prints random as double
-		System.out.println((int)random); // Prints random as int, Note the downcast
+//		generate 4 numbers
+		for (int i = 0; i < 4; i++) {
+//		generate one random number, print as ((int)random
+			double random = Math.random() * 6 + 1;		
+			System.out.println((int)random); // Prints random as int, Note the downcast	
+		}
 //		convert array to string tipToString() exists
 //		save Tip to Array int[] arrayTip
 	}
