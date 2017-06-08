61c242f7a47abd3a5d8b2b3eedea36f9f2da2b82
i20100 <20100@gmx.ch>
2017-06-08 14:46:34 +0200

    update mastermind
    changed code generator to write the code directly to the variable int[]
    odeArray

--- JAV-ZF-II/src/test/Mastermind.java
+++ JAV-ZF-II/src/test/Mastermind.java
@@ -1,12 +1,14 @@
 package test;
 
 public class Mastermind {
-
+	static int[] codeArray = {0, 0, 0, 0}; // Init Variable Code
+	
 	public static void main(String[] args) {
-//		generate 4 numbers
-		for (int i = 0; i < 4; i++) {
-//		generate one random number, print as ((int)random
-			double random = Math.random() * 6 + 1;		
+
+//		generate 4 numbers and write them to codeArray
+		for (int i = 0; i < codeArray.length; i++) {
+			double random = Math.random() * 6 + 1;
+			codeArray[i] = (int)random;
 			System.out.println((int)random); // Prints random as int, Note the downcast	
 		}
 //		convert array to string tipToString() exists
