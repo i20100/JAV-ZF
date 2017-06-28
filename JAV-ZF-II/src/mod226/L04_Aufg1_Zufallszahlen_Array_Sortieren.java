d65ce2ef428301a86806af3a95d0884f004db2ee
i20100 <20100@gmx.ch>
2017-06-28 17:11:40 +0200

    update 226-4-1 random number with array

--- JAV-ZF-II/src/mod226/L04_Aufg1_Zufallszahlen_Array_Sortieren.java
+++ JAV-ZF-II/src/mod226/L04_Aufg1_Zufallszahlen_Array_Sortieren.java
@@ -5,11 +5,12 @@ import java.util.Random;
 public class L04_Aufg1_Zufallszahlen_Array_Sortieren {
 
 	public static void main(String[] args) {
-		/* Generiere 30 Zahlen zwischen 1-50
-		Speichere diese im Array und gib diese aus
-		Sortiere nun diese Zahlen und gib sie aus
+		/* 
+		 * TODO Generiere 30 Zahlen zwischen 1-50
+		TODO Speichere diese im Array und gib diese aus
+		TODO Sortiere nun diese Zahlen und gib sie aus
 		An wie vielen Positionen stand im alten Array eine grössere Zahl?
-		Vergleiche die Positionen einzeln: altes Array > neue Position
+		TODO Vergleiche die Positionen einzeln: altes Array > neue Position
 		Konsolenausgabe siehe AB226-04.docx Aufg1 */
 		
 //		Get Random Number from 1-50
