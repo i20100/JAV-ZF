0d4be4b4e1a6bafed89a7ffbeed97d9baac70863
i20100 <20100@gmx.ch>
2017-07-05 21:11:39 +0200

    update mastermind
    changed Scanner to get tip number as a whole and not single digits
    separated with enters -> before x, Enter, x2, Enter ...
    Now -> XXXX Enter

--- JAV-ZF-II/src/publish/Mastermind.java
+++ JAV-ZF-II/src/publish/Mastermind.java
@@ -4,42 +4,103 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Mastermind {
-	/** Initial variables needed for the game  
+	/**
+	 * Initial variables needed for the game
 	 * 
-	 * codeArray = variable for saving the computer generated code
-	 * tipArray = variable for saving the user input
-	 * versuche = number of tries for the user to find the code
-	 * zaehler = counter for tries of user to find the code
+	 * codeArray = variable for saving the computer generated code tipArray =
+	 * variable for saving the user input versuche = number of tries for the
+	 * user to find the code zaehler = counter for tries of user to find the
+	 * code
 	 */
-	static int[] codeArray = {0, 0, 0, 0};
-	static int[] tipArray = {0, 0, 0, 0};
+	public static int[] codeArray = { 0, 0, 0, 0 };
+	static int[] tipArray = { 0, 0, 0, 0 };
 	static int versuche = 10;
 	static int zaehler = 0;
 
-
-	/** Dies ist das Spiel Masterminds.
+	/**
+	 * Dies ist das Spiel Mastermind.
 	 * <p>
-	 * Spielbeschreibung:
-	 * Computer wählt aus 6 möglichen Farben (Zahlen 1-6) 4 aus. 
-	 * Der Spieler versucht diesen "Code" zu erraten.
-	 * Dazu gibt er eine Kombination der 6 möglichen Zahlen ein.
-	 * Das Programm vergleicht die Eingabe mit dem "Code" und
-	 * wertet diese aus. Ist der "Code" geknackt oder die Anzahl versuche
-	 * aufgebraucht wird das Spiel beendet.
+	 * Spielbeschreibung: Computer wählt aus 6 möglichen Farben (Zahlen 1-6) 4
+	 * aus. Der Spieler versucht diesen "Code" zu erraten. Dazu gibt er eine
+	 * Kombination der 6 möglichen Zahlen ein. Das Programm vergleicht die
+	 * Eingabe mit dem "Code" und wertet diese aus. Ist der "Code" geknackt oder
+	 * die Anzahl versuche aufgebraucht wird das Spiel beendet.
 	 * <p>
+	 * 
 	 * @param args
+	 * @throws IOException 
 	 */
 	public static void main(String[] args) {
-		// TODO make it work as a runnable .jar! which opens console? or
-		// TODO make runnable exe
-		// TODO give tip history before every tip (array(arraytipnr(tip,amp,aop))
-		// TODO make input possible as one number of 4 digits
-		// TODO commit git when last version of CLI only
-		// TODO make a GUI with colors to click and submit
-		generateCode(codeArray);
-		System.out.println("Willkommen bei Masterminds"+"\n"+"\n"
-				+ "Sie haben "+ versuche + " Versuche um den Code zu knacken."+"\n"
-				+ "Der Code besteht aus den Zahlen 1-6. Der Code besteht aus "+"\n"
+		//TODO handle InputMismatchExeption with try {} catch (){}!
+		//TODO re-implement generate code!
+		//TODO beautiful menu from menu.jpage implement the like into mastermind?
+		// [1] Spiel starten
+		// [2] Spieloptionen -> todo add <b> ** debugg on / off option ** </b> 
+		//TODO Revert to last commit? new class GenerateCode not usable, stick with code in file for now!
+		//TODO make runnable exe
+		//TODO give tip history before every tip (array(arraytipnr(tip,amp,aop))
+		//TODO make tiphistory as table? https://stackoverflow.com/questions/15215326/how-can-i-create-table-using-ascii-in-a-console
+		//TODO make input possible as one number of 4 digits 
+		//(shoulb be possible easy way with space, since java recognizes space as separator)
+		//TODO commit git when last version of CLI only
+		//TODO release game for friends
+		//TODO make a GUI with colors to click and submit
+		//TODO implement missing Game requirements from doc AB226A-01-1.docx:
+		//TODO	Da auf der Konsole keinerlei grafische Ausgabe möglich ist, sollen die Farben durch einzelne Buchstaben dargestellt werden:
+		//
+		//			r	Rot
+		//			b	Blau
+		//			g	Grün
+		//			y	Gelb
+		//			w	Weiss
+		//			s	Schwarz
+		//
+		//TODO			Die Eingabe des Spielers soll jeweils auf einer neuen Zeile eingelesen werden. Sie soll aus einer Folge von vier Buchstaben ohne Zwischenraum bestehen und wird mit der Return-Taste abgeschlossen.
+		//
+		//TODO			Das Programm soll die Benutzereingabe auf Korrektheit überprüfen (validieren) und im Falle einer fehlerhaften Eingabe eine entsprechende Meldung ausgeben und zur Wiederholung der Eingabe auffordern.
+		//
+		//TODO			Bei einer gültigen Eingabe soll auf einer neuen Zeile die Bewertung der Eingabe ausgegeben werden (Schritt 3 in obigem Ablauf).
+		//
+		//TODO			Die Ausgaben des Programms sollen so gestaltet werden, dass dem Benutzer jederzeit klar ist, was von ihm erwartet wird (Benutzerfreundlichkeit).
+
+
+		//		 generateCode(codeArray); // whole Method has blockcomment
+		// GenerateCode();
+
+		// GenerateCode genCode = new GenerateCode();
+		// genCode.genCode(codeArray);
+
+		/*
+		 * Horse p0 = new Horse(); Horse p1 = new Horse(); p1.move();
+		 */
+
+		System.out.println("::::	 ::::     :::      :::::::: ::::::::::: :::::::::: :::::::::");  
+		System.out.println("*:*:*: :*:*:*   :*: :*:   :*:    :*:    :*:     :*:        :*:    :*:"); 
+		System.out.println("*:* *:*:* *:*  *:*   *:*  *:*           *:*     *:*        *:*    *:*"); 
+		System.out.println("*#*  *:*  *#* *#**:**#**: *#**:**#**    *#*     *#**:**#   *#**:**#: "); 
+		System.out.println("*#*       *#* *#*     *#*        *#*    *#*     *#*        *#*    *#*"); 
+		System.out.println("#*#       #*# #*#     #*# #*#    #*#    #*#     #*#        #*#    #*#"); 
+		System.out.println("###       ### ###     ###  ########     ###     ########## ###    ###"); 
+		System.out.println();
+		System.out.println("          ::::    ::::  ::::::::::: ::::    ::: :::::::::            "); 
+		System.out.println("          *:*:*: :*:*:*     :*:     :*:*:   :*: :*:    :*:           "); 
+		System.out.println("          *:* *:*:* *:*     *:*     :*:*:*  *:* *:*    *:*           "); 
+		System.out.println("          *#*  *:*  *#*     *#*     *#* *:* *#* *#*    *:*           "); 
+		System.out.println("          *#*       *#*     *#*     *#*  *#*#*# *#*    *#*           "); 
+		System.out.println("          #*#       #*#     #*#     #*#   #*#*# #*#    #*#           "); 
+		System.out.println("          ###       ### ########### ###    #### #########           ");
+		System.out.println();
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("\n\n             Drücke Enter um fortzufahren..");
+		String next = sc.nextLine();
+		//		String next = sc.next(); does not work!! but sc.nextLine(); gets the Enter =)
+		//TODO close scanner
+		//TODO clear console, this is a hard one
+
+		System.out.println("Willkommen bei Mastermind" + "\n" + "\n" + 
+				"Sie haben " + versuche+ " Versuche um den Code zu knacken." + "\n"
+				+ "Der Code besteht aus den Zahlen 1-6. Der Code besteht aus " + "\n"
 				+ "vier Stellen. Viel Erfolg!"
 				+ "\n");
 
@@ -47,79 +108,86 @@ public class Mastermind {
 			tipUser(tipArray);
 			tipEqualCode();
 			evalUserInput();
-			debugger();
+			debugger(); // Uncomment this line for exit debug mode aka hide code
 
 		} while (versuche > zaehler);
-		System.out.println("Keine Versuche mehr übrig. "
-				+ "Sie haben den Code nicht geknackt. "
-				+ "Das Spiel ist beendet...");
+		System.out.println(
+				"Keine Versuche mehr übrig. " + "Sie haben den Code nicht geknackt."
+						+ " " + "Das Spiel ist beendet...");
 		exit();
 	}
 
+
+	/*
+	 * private static void generateCode(int[] codeArray2) { /** Generates the
+	 * "Code" (Geheimcode) which the User has to guess.
+	 * 
+	 * @param codeArray2
+	 */ /*
+	 * // generate 4 numbers and write them to codeArray for (int i = 0; i <
+	 * codeArray.length; i++) { double random = Math.random() * 6 + 1;
+	 * codeArray[i] = (int)random; } }
+	 */
+
 	private static void exit() {
 		System.exit(1);
-		} 
-
-	/** Calls up the functions to evaluate the user input and displays the results.
-	 * 
-	 *  Calls functions:
-	 *  berechneAnrRichtigeOhnePos
-	 *  berechneAnzRichtiePosition
-	 */
-	private static void evalUserInput() {
-		System.out.println("Anzahl richtige Zahlen ohne Betrachtung der Position: "
-				+ berechneAnrRichtigeOhnePos(tipArray));
-		System.out.println("Anzahl mit richtiger Position: "
-				+ berechneAnzRichtiePosition(tipArray));
-		System.out.println("Danke für den " + zaehler + ". Tip. "
-				+ "Sie haben noch " + (versuche-zaehler) + " versuche übrig.");		
 	}
 
-	/** Use this for cheating, testing or debugging
+	/**
+	 * Calls up the functions to evaluate the user input and displays the
+	 * results.
+	 * 
+	 * Calls functions: berechneAnrRichtigeOhnePos berechneAnzRichtiePosition
+	 */
+	private static void evalUserInput() {
+		System.out.println(
+				"Anzahl richtige Zahlen ohne Betrachtung der Position: " 
+						+ berechneAnrRichtigeOhnePos(tipArray));
+		System.out.println("Anzahl mit richtiger Position: " 
+				+ berechneAnzRichtiePosition(tipArray));
+		System.out.println(
+				"Danke für den " + zaehler + ". Tip. " + "Sie haben noch " 
+						+ (versuche - zaehler) + " versuche übrig.");
+	}
+
+	/**
+	 * Use this for cheating, testing or debugging
 	 * 
 	 * Uncomment the two lines below to see the code and user input as Strings
 	 * 
 	 */
 	private static void debugger() {
-		//			System.out.println(arrayToString(codeArray));
-		//			System.out.println(arrayToString(tipArray));		
+		System.out.println(arrayToString(codeArray));
+		System.out.println(arrayToString(tipArray));
 	}
 
-	/** Evaluates if user has found out the code.
+	/**
+	 * Evaluates if user has found out the code.
 	 * 
 	 * If so jumps to endGame().
-	 * @return 
+	 * 
+	 * @return
 	 */
-	private static  void tipEqualCode() {
+	private static void tipEqualCode() {
 		boolean areEqual = Arrays.equals(codeArray, tipArray);
 		if (areEqual == true) {
 			endGame();
 		}
 	}
 
-	/** Terminates game.
+	/**
+	 * Terminates game.
 	 * 
 	 */
 	private static void endGame() {
-		System.out.println("Glückwunsch Sie sind ein Mastermind!!"
-				+ " Spiel beendet.");
+		System.out.println("Glückwunsch Sie sind ein Mastermind!!" + " Spiel beendet.");
 		exit();
 	}
 
-	/** Generates the "Code" (Geheimcode) which the User has to guess.
-	 * @param codeArray2
-	 */
-	private static void generateCode(int[] codeArray2) {
-		//		generate 4 numbers and write them to codeArray
-		for (int i = 0; i < codeArray.length; i++) {
-			double random = Math.random() * 6 + 1;
-			codeArray[i] = (int)random;
-		}		
-	}
-
 	/***
-	 * Berechnet die Anzahl der erratenen Zahlen aus dem Geheimcode ohne die 
-	 * Position der Zahlen zu berücksichtigen 
+	 * Berechnet die Anzahl der erratenen Zahlen aus dem Geheimcode ohne die
+	 * Position der Zahlen zu berücksichtigen
+	 * 
 	 * @param tip
 	 * @return int Anzahl erratene/vorhandene Zahlen
 	 */
@@ -131,30 +199,34 @@ public class Mastermind {
 				if (tip[i] == codeArray[j]) {
 					anzRichtige = anzRichtige + 1;
 					j = 4; // => Exit bei Treffer
-				} anzRichtige = anzRichtige + 0;
+				}
+				anzRichtige = anzRichtige + 0;
 			}
 		}
 		return anzRichtige;
 	}
-	
+
 	/**
 	 * Berechnet die Anzahl der erratenen Zahlen auf der richtigen Position
+	 * 
 	 * @param tip
 	 * @return int Anzahl Zahlen Position richtig
 	 */
 	public static int berechneAnzRichtiePosition(int[] tip) {
 		int anzRichtige = 0;
 		for (int i = 0; i < 4; i++) {
-			//			System.out.println("test");
+			// System.out.println("test");
 			if (tip[i] == codeArray[i]) {
 				anzRichtige = anzRichtige + 1;
-			} 
-			//			anzRichtige = anzRichtige + 0;
+			}
+			// anzRichtige = anzRichtige + 0;
 		}
 		return anzRichtige;
 	}
 
-	/** Converts any Int[] array to a single String
+	/**
+	 * Converts any Int[] array to a single String
+	 * 
 	 * @param array
 	 * @return
 	 */
@@ -166,19 +238,38 @@ public class Mastermind {
 		return arrayToString;
 	}
 
-	/** Ask User for his guess and gets the Userinput. 
+	/**
+	 * Ask User for his guess and gets the Userinput.
+	 * 
 	 * @param tipArray
 	 */
 	public static void tipUser(int[] tipArray) {
-		System.out.println("Bitte geben Sie vier Zahlen zwischen 1-6 nach "
-				+ "einander ein. Jede Zahl gefolgt von Enter.");
-		// TODO Zahleneingabe ohne Enter nach jeder Zahl ermöglichen
-		// TODO close reader
+		//TODO implement try, catch Exception handler
+		//DONE Solve question, if it is possible to get each int separate from
+		// a int like 1980 => 1, 9, 8, 0 ???
+		// ANSWER: => YES, see class ScannerIntToSingleInt.java
+		System.out.println(
+				"Bitte geben Sie vier Zahlen zwischen 1-6 nach " 
+						+ "einander ein gefolgt von Enter. Bsp. XXXX->Enter");
+		// DONE Zahleneingabe ohne Enter nach jeder Zahl ermöglichen
+		//FIXME Tip can be given in one number but the whole game doesnt work anymore, 
+		// rework game to work with new tip as whole number given!!
+		// DONE close reader
 		Scanner reader = new Scanner(System.in);
-		for (int i = 0; i < tipArray.length; i++) {
-			tipArray[i] = reader.nextInt();
-		}
-		zaehler = zaehler + 1; 
-	}
 
+		int tipUser = reader.nextInt();
+		System.out.println(tipUser); // just for testing!
+
+		String tipToDigits = String.valueOf(tipUser);
+		for(int i = 0; i < tipToDigits.length(); i++) {
+			int j = Character.digit(tipToDigits.charAt(i), 10);
+			System.out.println("digit: " + j);
+			//TODO write each digit into new Array[][][] when working with new usertiparray
+
+//			tipArray[zaehler] = reader.nextInt(); 
+
+			zaehler = zaehler + 1;
+		}
+		reader.close();
+	}
 }
