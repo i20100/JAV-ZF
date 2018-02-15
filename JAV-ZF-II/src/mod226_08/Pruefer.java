f1f8359ea43e60b2107e92c404e5ba1671a05358
i20100 <20100@gmx.ch>
2018-02-15 03:34:26 +0100

    init JUnit Testcases

---
+++ JAV-ZF-II/src/mod226_08/Pruefer.java
@@ -0,0 +1,52 @@
+package mod226_08;
+
+public class Pruefer {
+
+	private int zahl;
+	private boolean teilbarDrei;
+	private boolean teilbarFuenf;
+	private String zahlString;
+
+	public Pruefer(int i) {
+		this.zahl = i;
+		this.zahlString = Integer.toString(i);
+		teilbarDrei();
+		teilbarFuenf();
+	}
+
+	public int getZahl() {
+		return zahl;
+	}
+
+	boolean teilbarDrei() {
+		if (zahl % 3 == 0 ) {
+			teilbarDrei = true;
+			return true;
+		} 
+		return false;
+	}
+
+	public boolean teilbarFuenf() {
+		if (zahl % 5 == 0 ) {
+			teilbarFuenf = true;
+			return true;
+		} 
+		return false;
+	}
+
+	public String Antwort() {
+		// TODO Auto-generated method stub
+		if (teilbarDrei && teilbarFuenf == true) {
+			return "Fizz Buzz";
+		} else if (teilbarDrei == true && teilbarFuenf == false) {
+			return "Fizz";
+		} else if (teilbarDrei == false && teilbarFuenf == true) {
+			return "Buzz";
+		}
+		return zahlString;
+	}
+
+
+
+
+}
---
+++ JAV-ZF-II/src/mod226_08/Zahlenreihe.java
@@ -0,0 +1,12 @@
+package mod226_08;
+
+public class Zahlenreihe {
+
+	private int zahl = 0;
+
+	public int getZahl() {
+		zahl = zahl+1;
+		return zahl;
+	}
+
+}
