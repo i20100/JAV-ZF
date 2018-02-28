3136ec58c6c6c6c30dd5d7ca0867e4872fad1162
i20100 <20100@gmx.ch>
2018-02-28 21:40:10 +0100

    End of Chap 2, code refactored and reflected on
    changes made added new class Movie and swiched the counting logic to this class. shortend tests of Customer to comply with new duty.

--- JAV-MOD226-1/src/testgetriebeneentwicklungbuch/kap2/Customer.java
+++ JAV-MOD226-1/src/testgetriebeneentwicklungbuch/kap2/Customer.java
@@ -2,15 +2,9 @@ package testgetriebeneentwicklungbuch.kap2;
 
 public class Customer {
 	private double totalCharge = 0;
-	private static final double BASE_PRICE = 2.00; // Euro
-	private static final double PRICE_PER_DAY = 1.75; // Euro
-	private static final int DAYS_DISCOUNTED = 2;
 
 	public void rentMovie(int daysRented) {
-		totalCharge = totalCharge + BASE_PRICE;
-		if (daysRented > DAYS_DISCOUNTED) {
-			totalCharge = totalCharge + (daysRented - DAYS_DISCOUNTED) * 1.75;
-		}
+	totalCharge = totalCharge + Movie.getCharge(daysRented);
 	}
 
 	public double getTotalCharge() {
--- JAV-MOD226-1/src/testgetriebeneentwicklungbuch/kap2/CustomerTest.java
+++ JAV-MOD226-1/src/testgetriebeneentwicklungbuch/kap2/CustomerTest.java
@@ -2,44 +2,36 @@ package testgetriebeneentwicklungbuch.kap2;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CustomerTest {
-
-	@Test
-	void testRentingOneMovie() {
-		Customer customer = new Customer(); //Instanz mit Customerkonstruktor erstellen
-		customer.rentMovie(1); // Ausleihe ein Tag
-		assertTrue(customer.getTotalCharge() == 2.00);
+	private Customer customer;
+	
+	@BeforeEach
+	public void setUp() {
+	customer = new Customer();  //Instanz mit Customerkonstruktor erstellen
 	}
-
+	
 	@Test
-	void testRentingTwoMovies() {
-		Customer customer = new Customer(); //Instanz mit Customerkonstruktor erstellen
-		customer.rentMovie(1); // Ausleihe ein Tag
-		customer.rentMovie(2); // Ausleihe zwei Tage
-		assertEquals(4.00, customer.getTotalCharge(), 0.001);
-	}
-
-	@Test
-	void testRentingThreeMoviesMoreThanTwoDays() {
-		Customer customer = new Customer(); //Instanz mit Customerkonstruktor erstellen
-		customer.rentMovie(1); // Ausleihe ein Tag
-		customer.rentMovie(2); // Ausleihe zwei Tage
-		customer.rentMovie(3); // Ausleihe drei Tage (2+1.75=3.75)
-		assertEquals(7.75, customer.getTotalCharge(), 0.001);
+	void testRentingNoMovie() {
+		assertEquals(0, customer.getTotalCharge(), 0.001);
 		// 0.001 ist die Toleranz bei Fliesskommazahlen
 	}
 
 	@Test
-	void testRentingFourMovies()  {
-		Customer customer = new Customer();
+	void testRentingOneMovie() {
 		customer.rentMovie(1); // Ausleihe ein Tag
-		customer.rentMovie(2); // Ausleihe zwei Tage
-		customer.rentMovie(3);
-		customer.rentMovie(4); // Ausleihe vier Tage
-		assertEquals(13.25, customer.getTotalCharge(), 0.001);
+		assertEquals(2, customer.getTotalCharge(), 0.001);
 	}
 
+
+	@Test
+	void testRentingThreeMovies()  {
+		customer.rentMovie(2); // Ausleihe zwei Tage
+		customer.rentMovie(3); // Asuleihe drei Tage
+		customer.rentMovie(4); // Ausleihe vier Tage
+		assertEquals(11.25, customer.getTotalCharge(), 0.001);
+	}
 
 }
