87e8dec1d26480dd1407bcdd3dc47869ef6c7e46
i20100 <20100@gmx.ch>
2018-03-03 18:51:49 +0100

    setup of the JUnit setUp() method for Tests with same init state

--- JAV-MOD226-1/src/mod226_08/Euro.java
+++ JAV-MOD226-1/src/mod226_08/Euro.java
@@ -1,14 +1,25 @@
 package mod226_08;
 
 public class Euro {
-	double amount;
+	//	private final long cents;
+	private long cents;
 
-	public Euro(double d) {
-		this.amount = d;
+	public Euro(double euro) {
+		cents = Math.round(euro * 100.0);
+	}
+
+	private Euro(long cents) {
+		this.cents = cents;
 	}
 
 	public double getAmount() {
-		return amount;
+		return cents / 100.0;
+	}
+
+	public Euro add(Euro other) {
+//				cents = this.cents + other.cents; // those two make old and new Euro the same object
+//				return other;*/
+		return new Euro(this.cents + other.cents);
 	}
 
 }
--- JAV-MOD226-1/src/testgetriebeneentwicklungbuch/kap3/EuroTest.java
+++ JAV-MOD226-1/src/testgetriebeneentwicklungbuch/kap3/EuroTest.java
@@ -2,6 +2,9 @@ package testgetriebeneentwicklungbuch.kap3;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
 
 //import static org.junit.Assert.assertEquals; // This is from an older JUnit
 //import static org.junit.jupiter.api.Assertions.assertEquals; // This is JUnit5
@@ -13,19 +16,40 @@ import org.junit.jupiter.api.Test;
 import mod226_08.Euro;
 
 class EuroTest {
-
+	// TODO prüfe add() mit Minusbeträgen, => Name .add unzutreffend?
+	// TODO testRounding...() refaktorisieren, da keine Verwendung von 'Euro two = new Euro(2.00);'
+	private Euro two;
+	
+	@BeforeEach
+	protected void setUp() {
+		two = new Euro(2.00);
+	}
+	
+	
 	@Test
-	void testGetAmount() {
-		Euro twoEuro = new Euro(2);
-//		assertEquals("getAmount wrong", 2.00, twoEuro.getAmount(), 0.001); // Bsp. geht nicht mit JUnit5 import!
-//		assertEquals(2.00, twoEuro.getAmount(), 0.001); //Dieses Bsp. geht mit JUnit5 import!
-		assertEquals("getAmount wrong", 2.00, twoEuro.getAmount(), 0.001);
+	public void testAdding() {
+		Euro sum = two.add(two);
+		assertEquals("sum", 4.00, sum.getAmount(), 0.001);
+		assertEquals("two", 2.00, two.getAmount(), 0.001);
 	}
 
 	@Test
-	public void testRounding() {
+	void testGetAmount() {
+		//		assertEquals("getAmount wrong", 2.00, twoEuro.getAmount(), 0.001); // Bsp. geht nicht mit JUnit5 import!
+		//		assertEquals(2.00, twoEuro.getAmount(), 0.001); //Dieses Bsp. geht mit JUnit5 import!
+		assertEquals("getAmount wrong", 2.00, two.getAmount(), 0.001);
+	}
+
+	@Test
+	public void testRoundingUP() {
 		Euro rounded = new Euro(1.995);
-		assertTrue("Rounding getAmount wrong", 2.00 == rounded.getAmount());
+		assertEquals("RoundingUP getAmount wrong", 2.00, rounded.getAmount(), 0.001);
+	}
+
+	@Test
+	public void testRoundingDOWN() {
+		Euro rounded = new Euro(1.994);
+		assertEquals("RoundingDOWN getAmount wrong", 1.99, rounded.getAmount(), 0.001);
 	}
 
 
