5f1a10c07e056d5503ec5ca2039b0f32ffd27e68
i20100 <20100@gmx.ch>
2018-03-18 12:09:28 +0100

    implementation of new times method aka refactoring

--- JAV-MOD226-1/src/testgetriebeneentwicklungbuch/kap4/RegularPrice.java
+++ JAV-MOD226-1/src/testgetriebeneentwicklungbuch/kap4/RegularPrice.java
@@ -9,30 +9,28 @@ public class RegularPrice {
 	private static final int DAYS_DISCOUNTED = 3;
 
 	public Euro getCharge(int daysRented) {
-//		new Code fails from book
+		//		new Code fails from book
 		//		Euro result = new Euro(1.50);
-//		if (daysRented == 4) result.add(new Euro(1.50));
-//		return result;
-		
-////		this is not considered good, cause it does not represent the complexity
-////		of having a base price and an increment
-////		so in the example it gets changed with code further below
-//		if (daysRented <= 3) return new Euro(1.50);
-//		return new Euro(3.00);
-		
-//		if (daysRented <= 3) return new Euro(1.50);
-//		return new Euro(1.50).add(new Euro(1.50));
-		
+		//		if (daysRented == 4) result.add(new Euro(1.50));
+		//		return result;
+
+		////		this is not considered good, cause it does not represent the complexity
+		////		of having a base price and an increment
+		////		so in the example it gets changed with code further below
+		//		if (daysRented <= 3) return new Euro(1.50);
+		//		return new Euro(3.00);
+
+		//		if (daysRented <= 3) return new Euro(1.50);
+		//		return new Euro(1.50).add(new Euro(1.50));
+
 		// Good code needs good names
-//		if (daysRented <= DAYS_DISCOUNTED) return BASERPICE;
-//		return BASERPICE.add(PRICE_PER_DAY);
-		
+		//		if (daysRented <= DAYS_DISCOUNTED) return BASERPICE;
+		//		return BASERPICE.add(PRICE_PER_DAY);
+
 		if (daysRented <= DAYS_DISCOUNTED) return BASERPICE;
-		
+
 		int additionalDays = daysRented - DAYS_DISCOUNTED;
-		return BASERPICE.add(new Euro(PRICE_PER_DAY.getAmount() * additionalDays));
-		
-		
+		return BASERPICE.add(PRICE_PER_DAY.times(additionalDays));
 	}
 
 }
