d1d715a6a4e4e6bcb3de85b200875d7faedaac06
i20100 <20100@gmx.ch>
2018-10-15 21:12:49 -0100

    state at the end of taks 2 of mod226B_02
    expanded classes with constructors for super class and sub class. learned how to use contructors with super and sub class.

--- JAV-MOD226B/src/mod226B_02/Personen.java
+++ JAV-MOD226B/src/mod226B_02/Personen.java
@@ -1,38 +1,76 @@
 package mod226B_02;
 
 class Person {
-  String name;
-  String vorname;
-  int personalNummer;
+	protected String name;
+	protected String vorname;
+	protected int personalNummer;
+
+	public Person(String name, String vorname, int personalNummer) {
+		this.name = name;
+		this.vorname = vorname;
+		this.personalNummer = personalNummer;
+	}
+
 }
 
 class Chef extends Person {
-  String abteilung;
+	private String abteilung;
+
+	public Chef(String name, String vorname, int personalNummer, String abteilung) {
+		super(name, vorname, personalNummer);
+		this.setAbteilung(abteilung);
+	}
+
+	/**
+	 * @return the abteilung
+	 */
+	public String getAbteilung() {
+		return abteilung;
+	}
+
+	/**
+	 * @param abteilung the abteilung to set
+	 */
+	public void setAbteilung(String abteilung) {
+		this.abteilung = abteilung;
+	}
 }
 
 class Fachangestellter extends Person {
-  Chef vorgesetzter;
+	private Chef vorgesetzter;
+
+	public Fachangestellter(String name, String vorname, int personalNummer, Chef vorgesetzter) {
+		super(name, vorname, personalNummer);
+		this.setVorgesetzter(vorgesetzter);
+	}
+
+	/**
+	 * @return the vorgesetzter
+	 */
+	public Chef getVorgesetzter() {
+		return vorgesetzter;
+	}
+
+	/**
+	 * @param vorgesetzter the vorgesetzter to set
+	 */
+	public void setVorgesetzter(Chef vorgesetzter) {
+		this.vorgesetzter = vorgesetzter;
+	}
+
 }
 
 
 public class Personen {
 	public static void main(String[] args) {
-	  Fachangestellter personal1 = new Fachangestellter();
-	  @SuppressWarnings("unused")
-	Fachangestellter personal2 = new Fachangestellter();
-	  Chef personal3 = new Chef();
-	  Person personal4 = new Person();
+		Chef personal3 = new Chef("Klein", "Thomas", 2, "Verkauf");
+		Fachangestellter personal1 = new Fachangestellter("Sattler", "Beatrice", 1, personal3);
+		Person personal4 = new Person("Meier", "Hans", 3);
 
-	  personal3.name="Sattler";
-	  personal3.vorname="Beatrice";
-	  personal3.abteilung="Verkauf";
-	  personal1.name="Klein";
-	  personal1.vorname="Thomas";
-
-	  personal1.vorgesetzter=personal3; 		//*1
-	  personal4 = personal1; 				//*2
-//	  personal4.vorgesetzter = personal3; 		//*3 -> Fehler
-//	  personal1 = personal4; 				//*4 -> Fehler
-	  personal1 = (Fachangestellter) personal4; 	//*5
-  }
+		personal1.setVorgesetzter(personal3); 		//*1
+		personal4 = personal1; 				//*2
+		//	  personal4.vorgesetzter = personal3; 		//*3 -> Fehler
+		//	  personal1 = personal4; 				//*4 -> Fehler
+		personal1 = (Fachangestellter) personal4; 	//*5
+	}
 }
