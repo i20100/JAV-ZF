247d7f794486b6032e9863a8127a2951372d46e1
i20100 <20100@gmx.ch>
2018-10-20 20:06:45 -0100

    state at end of task 1, chapter 3, inherit methods override and dynamic binding

--- JAV-MOD226B/src/mod226B_02/Personen.java
+++ JAV-MOD226B/src/mod226B_02/Personen.java
@@ -22,7 +22,7 @@ class Firma {
 	}
 }
 
-class Person {
+abstract class Person {
 	protected String name;
 	protected String vorname;
 	protected int personalNummer;
@@ -33,6 +33,12 @@ class Person {
 		this.personalNummer = personalNummer;
 	}
 
+	public void print() {
+		System.out.println("Personal-Nr.: "+personalNummer+"\n"
+				+ "Name und Vorname : "+name+" "+vorname);
+	}
+
+	public abstract int berechneFerien(int alter);
 }
 
 class Chef extends Person {
@@ -42,6 +48,25 @@ class Chef extends Person {
 		super(name, vorname, personalNummer);
 		this.setAbteilung(abteilung);
 	}
+
+	@Override
+	public void print() {
+		super.print();
+		System.out.println("Abteilungsleiter fuer: "+ abteilung);
+	}
+
+	@Override
+	public int berechneFerien(int alter) {
+		int ferien = 4;
+		if (alter > 54) {
+			ferien = 6;
+		}
+		if (alter > 44) {
+			ferien = 5;
+		}
+		return ferien;
+	}
+
 
 	/**
 	 * @return the abteilung
@@ -80,23 +105,76 @@ class Fachangestellter extends Person {
 		this.vorgesetzter = vorgesetzter;
 	}
 
+	@Override
+	public int berechneFerien(int alter) {
+		int ferien = 4;
+		if (alter > 59) {
+			ferien = 6;
+		}
+		if (alter > 49) {
+			ferien = 5;
+		}
+		return ferien;
+	}
+
 }
 
+class Lernender extends Person {
+	private int lehrjahr;
+
+	public Lernender(String name, String vorname, int personalNummer, int lehrjahr) {
+		super(name, vorname, personalNummer);
+		this.lehrjahr = lehrjahr;
+	}
+
+	@Override
+	public void print() {
+		super.print();
+		System.out.println("Lehrjahr: "+lehrjahr);
+	}
+
+	@Override
+	public int berechneFerien(int alter) {
+		int ferien = 5;
+		return ferien;
+	}
+}
 
 public class Personen {
 	public static void main(String[] args) {
 		@SuppressWarnings("unused")
 		Firma f = new Firma(
 				new Person[] {new Chef("Sattler", "Beatrice",25 , "Verkauf")} ); // <==
-// Teste die Aussage aus dem Video ein Cast von Kreis zu Rechteck geht nicht aber ein cast von Rechteck zu Rechteck geht und keine Daten gehen verloren?
-		
+		// Teste die Aussage aus dem Video ein Cast von Kreis zu Rechteck geht nicht aber ein cast von Rechteck zu Rechteck geht und keine Daten gehen verloren?
+
+
+
 		Chef chef1 = new Chef("Meier", "Hans", 1, "Einkauf");
 		Person p1;
 		p1 = chef1;
-		
+
 		Chef chef2;
 		chef2 = (Chef) p1;
 		System.out.println(chef2.getAbteilung());
 
+		//		f.setMitarbeiter(new Fachangestellter("von Siebenthal", "Marie", 5, f.getMitarbeiter()));
+	
+		Fachangestellter fa = new Fachangestellter("von Siebenthal", "Marie", 5, chef1);
+		Lernender l = new Lernender("Schweizer", "Kevin", 3, 2);
+
+		p1.print();
+		fa.print();
+		l.print();
+
+		Person[] angestellte = new Person[4];
+		angestellte[0] = chef1;
+		angestellte[1] = fa;
+		angestellte[2] = l;
+		angestellte[3] = new Fachangestellter("Schweizer", "Alois", 6, chef1);
+		
+		for (Person person : angestellte) {
+			System.out.println("Der Ferienanspruch beträgt: " + person.berechneFerien(46));
+		}
+		
 	}
 }
